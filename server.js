2317088ad1d3865edaa7e7cf1ad36c9e98dd2de3
Amitoj Singh <singhamitoj0410@gmail.com>
2023-01-20 16:50:56 -0500

    last

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send({message : "API Listening"});
+    res.send("hello");
 });
 
 app.post('/api/movies', (req, res) => {
